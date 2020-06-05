af08c543b761a5456fc6a532b0f951ac80bf5692
geftactics <geoff@squiggle.org>
2020-06-05 09:33:52 +0100

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,4 +1,8 @@
 resource "aws_s3_bucket" "example" {
-  bucket = var.s3_name
+  bucket = "${var.s3_name}-${random_id.s3.hex}"
   acl    = "private"
+}
+
+resource "random_id" "s3" {
+  byte_length = 8
 }
