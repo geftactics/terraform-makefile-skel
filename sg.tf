2a2cdc4ee203dda588fa336678b6ad6d4e31a476
geftactics <geoff@squiggle.org>
2020-06-05 09:16:39 +0100

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -1,7 +1,7 @@
 resource "aws_default_vpc" "default" {
 }
 
-resource "aws_security_group" "rtmp" {
+resource "aws_security_group" "example" {
   name   = var.sg_name
   vpc_id = aws_default_vpc.default.id
 }
